f7e889b72fdf822c22672eb9d20d9d36b90eebe4
Laura Chan <laurachan205@gmail.com>
2018-01-29 15:08:36 +0000

    Oystercard has top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -2,9 +2,20 @@ require 'oystercard'
 
 describe Oystercard do
 
+  subject(:oystercard) { described_class.new }
+
   context "when created" do
     it "has a balance of 0" do
       expect(subject.balance).to eq 0
+    end
+  end
+
+  it { is_expected.to respond_to(:top_up).with(1).argument}
+
+  describe '#top_up' do
+    it 'adds money to the balance' do
+      subject.top_up(100)
+      expect(subject.balance).to eq 100
     end
   end
 
